8c1089dcdddeb58f73119bd7671def0162aef1ea
hharpreetk <harpreetk.99@gmail.com>
2023-08-17 10:23:58 -0400

    added styles to the chart

--- pop_repos.py
+++ pop_repos.py
@@ -16,11 +16,17 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Process respository information.
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars= [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
 # Make visualization.
-fig = px.bar(x=repo_names, y=stars)
+title = "Most-Starred Python Projects on GitHub"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
+yaxis_title_font_size=20)
+
 fig.show()
