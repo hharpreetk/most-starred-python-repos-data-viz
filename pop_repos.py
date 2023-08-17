a65b38fabb2456968ffb5a67dfac91340f9f16b6
hharpreetk <harpreetk.99@gmail.com>
2023-08-17 10:02:39 -0400

    visuzalized repositories using plotly

--- pop_repos.py
+++ pop_repos.py
@@ -1,5 +1,7 @@
 import requests
 
+import plotly.express as px
+
 # Make an API call and check the response.
 url = "https://api.github.com/search/repositories"
 url += "?q=language:python+sort:stars+stars:>10000"
@@ -10,22 +12,15 @@ print(f"Status code: {r.status_code}")
 
 # Convert the response object to a dictionary.
 response_dict = r.json()
-
-# Process results.
-print(f"Total repositories: {response_dict['total_count']}")
 print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Explore information about the repositories.
+# Process respository information.
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
-
-# Examine the first repository.
-repo_dict = repo_dicts[0]
-
-print("\nSelected information about each repository:")
+repo_names, stars = [], []
 for repo_dict in repo_dicts:
-    print(f"\nName: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Description: {repo_dict['description']}")
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+fig = px.bar(x=repo_names, y=stars)
+fig.show()
