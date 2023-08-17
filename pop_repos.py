aabed6ab9b78a6bd2dd3a466d5930c9d156f2501
hharpreetk <harpreetk.99@gmail.com>
2023-08-17 13:25:52 -0400

    added clickable links

--- pop_repos.py
+++ pop_repos.py
@@ -16,9 +16,14 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Process respository information.
 repo_dicts = response_dict['items']
-repo_names, stars, hover_texts = [], [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    # Turn repo names into active links.
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
     stars.append(repo_dict['stargazers_count'])
 
     # Build hover texts.
@@ -30,7 +35,7 @@ for repo_dict in repo_dicts:
 # Make visualization.
 title = "Most-Starred Python Projects on GitHub"
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels, hover_name=hover_texts)
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts, color_discrete_sequence=px.colors.qualitative.Dark24_r)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
 yaxis_title_font_size=20)
